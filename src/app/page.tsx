89614cbacd2874ed624ee10b6456752a19d0e17e
first_name last_name <102787289+EarHengLeap@users.noreply.github.com>
2024-02-08 19:45:17 +0700

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
       <Card className="rounded-2xl bg-slate-950 shadow-2xl text-white text-center h-[760px]">
         <div className="mx-auto max-w-[683px] mt-[215px]">
           <h1 className="text-6xl font-bold leading-tight">
-            Welcome to <br/> Phnom Penh Digital University
+            Welcome to <br /> Phnom Penh Digital University
           </h1>
-          <p className="text-3xl mt-2">Let's Register your account</p>
+          <p className="text-3xl mt-2">Let&apos;s Register your account</p>
           <Button className="mt-12" variant={"secondary"} size={"lg"}>
             Learn More
           </Button>
